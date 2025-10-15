2006cc8053f1a61eb9107e2d0fc78214747e88bf
LEE JU EUN <wndms090909@gmail.com>
2025-10-15 14:33:31 +0900

    지도아이콘 변경

--- js/sub_store.js
+++ js/sub_store.js
@@ -27,35 +27,47 @@ closeBtn.addEventListener("click", (e) => {
 
 
 var mapContainer = document.getElementById('map'),
-    mapOption = { 
+  mapOption = { 
         center: new kakao.maps.LatLng(37.5618815, 126.8511103),
-        level: 3
+        level: 3 // 지도의 확대 레벨
     };
 
 var map = new kakao.maps.Map(mapContainer, mapOption);
 
-// 마커가 표시될 위치입니다 
-var markerPosition  = new kakao.maps.LatLng(37.5618815, 126.8511103); 
+var imageSrc = "../images/bhc-ms.svg",   
+    imageSize = new kakao.maps.Size(50, 50),
+    imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정
 
-// 마커를 생성합니다 
+// 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+    markerPosition = new kakao.maps.LatLng(37.5618815, 126.8511103); // 마커가 표시될 위치입니다
+
+// 마커를 생성합니다
 var marker = new kakao.maps.Marker({
-    position: markerPosition
+  position: markerPosition,
+  image: markerImage // 마커이미지 설정 
 });
 
 // 마커가 지도 위에 표시되도록 설정합니다
-marker.setMap(map);
+marker.setMap(map);  
 
-var iwContent = '<div style="padding-left:30px;">BHC 가양점 <br><a href="https://map.kakao.com/link/map/Hello World!,37.5618815, 126.8511103" </div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-    iwPosition = new kakao.maps.LatLng(37.5618815, 126.8511103); //인포윈도우 표시 위치입니다
+// 커스텀 오버레이에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+var content = '<div class="customoverlay">' +
+    '  <a href="https://map.kakao.com/link/map/26628353" target="_blank">' +
+    '    <span class="title">BHC가양점</span>' +
+    '  </a>' +
+    '</div>';
 
-// 인포윈도우를 생성합니다
-var infowindow = new kakao.maps.InfoWindow({
-    position : iwPosition, 
-    content : iwContent 
+// 커스텀 오버레이가 표시될 위치입니다 
+var position = new kakao.maps.LatLng(37.5618815, 126.8511103);  
+
+// 커스텀 오버레이를 생성합니다
+var customOverlay = new kakao.maps.CustomOverlay({
+    map: map,
+    position: position,
+    content: content,
+    yAnchor: 1 
 });
-  
-// 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
-infowindow.open(map, marker); 
 
 //지역선택
 cityBox.addEventListener("click", () => {
