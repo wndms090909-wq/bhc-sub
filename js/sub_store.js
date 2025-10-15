f10a9a4b82a1e4bb1f551d9a9b424d3f239b4598
LEE JU EUN <wndms090909@gmail.com>
2025-10-15 18:00:08 +0900

    팝업 반응형

--- js/sub_store.js
+++ js/sub_store.js
@@ -1,16 +1,25 @@
 
-let mapDetail = document.querySelector(".store");
+let storeItem = document.querySelector(".store");
 let popup = document.querySelector(".popup_wrap");
 let closeBtn = document.querySelector(".popup .close_btn");
 let cityBox = document.querySelector(".city_box");
 let countiesBox = document.querySelector(".counties_box");
 let cityList = document.querySelector(".city_list");
 let countiesList = document.querySelector(".counties_list");
+let mapDetail = document.querySelector(".store .detail");
 
-console.log(mapDetail);
+console.log(storeItem);
+
+mapDetail.addEventListener("mouseenter", () => {
+    mapDetail.classList.add("active");
+})
+
+mapDetail.addEventListener("mouseout", () => {
+    mapDetail.classList.remove("active");
+})
 
 //지도
-mapDetail.addEventListener("click", () => {
+storeItem.addEventListener("click", () => {
     popup.style.display = "flex";
 
 
@@ -27,28 +36,28 @@ closeBtn.addEventListener("click", (e) => {
 
 
 var mapContainer = document.getElementById('map'),
-  mapOption = { 
+    mapOption = {
         center: new kakao.maps.LatLng(37.5618815, 126.8511103),
         level: 3
     };
 
 var map = new kakao.maps.Map(mapContainer, mapOption);
 
-var imageSrc = "../images/bhc-ms.svg",   
+var imageSrc = "../images/bhc-ms.svg",
     imageSize = new kakao.maps.Size(50, 50),
-    imageOption = {offset: new kakao.maps.Point(27, 69)}; //마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정
+    imageOption = { offset: new kakao.maps.Point(27, 69) }; //마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정
 
 // 마커의 이미지정보를 가지고 있는 마커이미지를 생성
 var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
     markerPosition = new kakao.maps.LatLng(37.5618815, 126.8511103); // 마커가 표시될 위치
 
 var marker = new kakao.maps.Marker({
-  position: markerPosition,
-  image: markerImage // 마커이미지 설정 
+    position: markerPosition,
+    image: markerImage // 마커이미지 설정 
 });
 
 
-marker.setMap(map);  
+marker.setMap(map);
 
 
 var content = '<div class="customoverlay">' +
@@ -58,14 +67,14 @@ var content = '<div class="customoverlay">' +
     '</div>';
 
 // 커스텀 오버레이가 표시될 위치
-var position = new kakao.maps.LatLng(37.5618815, 126.8511103);  
+var position = new kakao.maps.LatLng(37.5618815, 126.8511103);
 
 // 커스텀 오버레이를 생성
 var customOverlay = new kakao.maps.CustomOverlay({
     map: map,
     position: position,
     content: content,
-    yAnchor: 1 
+    yAnchor: 1
 });
 
 //지역선택
@@ -81,3 +90,5 @@ countiesBox.addEventListener("click", () => {
 countiesList.addEventListener("click", () => {
     countiesList.style.display = "none";
 })
+
+
