02b5e09f69bc0e2dbcf9ffbc14e2a16d0297542d
LEE JU EUN <wndms090909@gmail.com>
2025-10-15 14:35:37 +0900

    js수정

--- js/sub_store.js
+++ js/sub_store.js
@@ -29,39 +29,38 @@ closeBtn.addEventListener("click", (e) => {
 var mapContainer = document.getElementById('map'),
   mapOption = { 
         center: new kakao.maps.LatLng(37.5618815, 126.8511103),
-        level: 3 // 지도의 확대 레벨
+        level: 3
     };
 
 var map = new kakao.maps.Map(mapContainer, mapOption);
 
 var imageSrc = "../images/bhc-ms.svg",   
     imageSize = new kakao.maps.Size(50, 50),
-    imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정
+    imageOption = {offset: new kakao.maps.Point(27, 69)}; //마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정
 
-// 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+// 마커의 이미지정보를 가지고 있는 마커이미지를 생성
 var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
-    markerPosition = new kakao.maps.LatLng(37.5618815, 126.8511103); // 마커가 표시될 위치입니다
+    markerPosition = new kakao.maps.LatLng(37.5618815, 126.8511103); // 마커가 표시될 위치
 
-// 마커를 생성합니다
 var marker = new kakao.maps.Marker({
   position: markerPosition,
   image: markerImage // 마커이미지 설정 
 });
 
-// 마커가 지도 위에 표시되도록 설정합니다
+
 marker.setMap(map);  
 
-// 커스텀 오버레이에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+
 var content = '<div class="customoverlay">' +
     '  <a href="https://map.kakao.com/link/map/26628353" target="_blank">' +
     '    <span class="title">BHC가양점</span>' +
     '  </a>' +
     '</div>';
 
-// 커스텀 오버레이가 표시될 위치입니다 
+// 커스텀 오버레이가 표시될 위치
 var position = new kakao.maps.LatLng(37.5618815, 126.8511103);  
 
-// 커스텀 오버레이를 생성합니다
+// 커스텀 오버레이를 생성
 var customOverlay = new kakao.maps.CustomOverlay({
     map: map,
     position: position,
